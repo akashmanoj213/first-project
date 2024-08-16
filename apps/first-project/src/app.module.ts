3c93cef100916d1925cd2c8f251608935d952d67
Akash Manoj <akashmanoj213@gmail.com>
2024-08-16 15:03:43 +0530

    fix: added .env config

--- apps/first-project/src/app.module.ts
+++ apps/first-project/src/app.module.ts
@@ -8,7 +8,7 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ envFilePath: './.env' }),
     PubSubClientModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
